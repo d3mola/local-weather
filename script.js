08405a61337f1202cb320e576ca66886221363ae
d3mola <ariyaademola@gmail.com>
2017-11-06 00:36:35 +0100

    Get geolocation data and display

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
 // use jquery
 $(document).ready(function() {
-	const endpoint = "https://fcc-weather-api.glitch.me/";
-	let url = endpoint;
-	let latitude = 40.7128, longitude = 74.0060;
-	url += "api/current?lon=" + longitude + "&lat=" + latitude;
-	$.getJSON(url, (data) => {
-		let location = "<p>" + data.name + "</p>";
-		let temperature = "<p>" + data.main.temp + "</p>";
-		$(".location").html(location);
-		$(".temperature").html(temperature);
-	});
+	// get geolocatipn data, then display
+	if (navigator.geolocation) {
+	  navigator.geolocation.getCurrentPosition(function(position) {
+			let latitude = position.coords.latitude;
+			let longitude = position.coords.longitude;
+
+			// set the url
+			const endpoint = "https://fcc-weather-api.glitch.me/";
+			let url = endpoint;
+			url += "api/current?lon=" + longitude + "&lat=" + latitude;
+
+			// get JSON data and display
+			$.getJSON(url, (data) => {
+				let location = "<p>" + data.name + "</p>";
+				let temperature = "<p>" + data.main.temp + " F" + "</p>";
+
+				// manipulate DOM to display JSON data
+				$(".location").html(location);
+				$(".temperature").html(temperature);
+			});
+	  });
+	}
+
+
 });
