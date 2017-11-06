3ad7be06bfa4d45a4dc8c24203d554e84246d2e1
d3mola <ariyaademola@gmail.com>
2017-11-06 08:37:05 +0100

    Toggle between two background colors as an example

--- script.js
+++ script.js
@@ -11,29 +11,55 @@ $(document).ready(function() {
 			let url = endpoint;
 			url += "api/current?lon=" + longitude + "&lat=" + latitude;
 
+			let unit = " C";
+
 			// get JSON data and display
 			$.getJSON(url, (data) => {
 				let location = "<p>" + data.name+ ", " + data.sys.country + "</p>";
-				let temperature = "<p>" + data.main.temp + " F" + "</p>";
-				temperature += "<br><p>" + data.weather[0].description + "</p>";
+				let temperature = "<p>" + data.main.temp + unit + "</p>";
+				let weather = "<p>" + data.weather[0].description + "</p>";
 
 				// manipulate DOM to display JSON data
-				$(".location").html(location);
-				$(".temperature").html(temperature);
-				//$("#data").html(JSON.stringify(data));
-			});
-	  });
-	}
+				$(".location").html(location); // display location
+				$("#temp").html(temperature); // display temperature and sky description
+				$("#sky").html(weather);
+				//$("#data").html(JSON.stringify(data)); // display full json data
+
+				let clicked = true;
+				// convert temp when button is clicked
+				$("#button").on("click", () => {
+					// once button is clicked, grab temperature and modify
+					if (clicked) {
+						$("body").css("background-color", "red");
+						clicked = false;
+					} else {
+						$("body").css("background-color", "green");
+						clicked = true;
+					}
+
+					//clicked = false;
+					/*
+					let farenheitTemp = tempConverter.toFarenheit(data.main.temp);
+					farenheitTemp = "<p>" + farenheitTemp + " F" + "</p>";
+					$("#temp").html(farenheitTemp);
+					*/
+
+					// far to cel
+					// let celciusTemp = tempConverter.toCelcius(data.main.temp);
+					// $(".temperature").html(JSON.stringify(celciusTemp) + " C");
+				});// end of btn click
+			});// end of getJSON from API
 
 
-});
+	  });// end of getCurrentPosition function
+	}// end of if navigator
+
+});// end of jquery
 
 // celcius to farenheit converter
-//const celcius = farenTemp => (farenTemp - 32) / 1.8;
-
 const tempConverter = {
-	celcius: farenTemp => (farenTemp - 32) / 1.8,
-	farenheit: celsTemp => (celsTemp * 1.8) + 32
+	toCelcius: farenTemp => (farenTemp - 32) / 1.8,
+	toFarenheit: celsTemp => (celsTemp * 1.8) + 32
 }
-console.log(`celcius: ${tempConverter.celcius(68)}`);
-console.log(`farenheit: ${tempConverter.farenheit(20)}`)
+// console.log(`celcius: ${tempConverter.toCelcius(26.63)}`);
+// console.log(`farenheit: ${tempConverter.toFarenheit(26.63)}`)
